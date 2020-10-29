a7ca686b571cdfa87b19876edc4801b166c33f05
unknown <hey@wptheme4.me>
2020-10-29 23:04:05 +0200

    apie-mus.pug nurodziau neteisinga kintamojo tipa

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     site_title: "uasociacija.eu",
     dev_siteurl: "http://localhost:3000/",
-    prod_siteurl: "https://uasociacija-eu.netlify.com/",
+    prod_siteurl: "https://uasociacija-eu.netlify.app/",
     prod_siteurl_fb_img: "https://uasociacija-eu.netlify.com/img/logo.png",
     google_analytics_key: "API-KEY",
     facebook_appid: "APP_ID",
